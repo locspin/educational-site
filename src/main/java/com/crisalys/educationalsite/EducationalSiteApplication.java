458a315511057f72c957e5551c01576f7c28b26b
Dwayne Sands <dcslocke@gmail.com>
2020-05-18 21:38:36 -0700

    educational site

---
+++ src/main/java/com/crisalys/educationalsite/EducationalSiteApplication.java
@@ -0,0 +1,13 @@
+package com.crisalys.educationalsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crisalys/educationalsite/EducationalSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisalys.educationalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
